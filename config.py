ee749f0139f59b2644e3f12f2250aeb1d9f7049a
Steven Lee <steven@alitrack.com>
2021-12-17 08:08:42 +0800

    clean requirements

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ SECRET_KEY = "\2\1thisismyscretkey\1\2\e\y\y\h"
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 # The SQLAlchemy connection string.
-SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "app.db")
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+# SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "app.db")
+# SQLALCHEMY_TRACK_MODIFICATIONS = False
